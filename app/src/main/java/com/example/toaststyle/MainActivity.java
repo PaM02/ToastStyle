a73f60a10dffe4d9d41d0bfdb6dd907e54935932
papa matar fall <matar026@gmail.com>
2022-10-24 13:54:35 +0000

    cv

---
+++ app/src/main/java/com/example/toaststyle/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.toaststyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.muddzdev.styleabletoast.StyleableToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StyleableToast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG, R.style.exampleToast).show();
+            }
+        });
+    }
+}
